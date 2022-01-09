8df3957a801cba11a06956c31ffc01620f3e4e62
Wish <i@itsse.cn>
2022-01-09 19:56:31 +0800

    Add files via upload

---
+++ blog/config.js
@@ -0,0 +1,24 @@
+let blogData = {
+    routers: [
+        {
+            name: "舔狗日记",
+            link: "http://dog.itsse.cn",
+            icon: ""
+        },
+        {
+            name: "m3u8解析",
+            link: "http://m3u8.itsse.cn",
+            icon: ""
+        },
+        {
+            name: "音乐文件解析",
+            link: "http://music.itsse.cn",
+            icon: ""
+        },
+        {
+            name: "图床",
+            link: "https://imgurl.icu",
+            icon: ""
+        }
+    ]
+}
